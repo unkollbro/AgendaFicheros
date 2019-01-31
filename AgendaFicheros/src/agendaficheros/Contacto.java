b977ce4290d97fe8274c5767f9995a054f4c0b82
Juanfran <Juanfran@Ternasco>
2019-01-31 17:47:20 +0100

    Contacto finalizado.

---
+++ AgendaFicheros/src/agendaficheros/Contacto.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agendaficheros;
+
+/**
+ *
+ * @author Juanfran
+ */
+public class Contacto {
+    private String nombre;
+    private String telefono;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " --- " + telefono;
+    }
+    
+    
+}
